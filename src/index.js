447b4d7033d9a6a9c6e01197c3fb6d2dd81e91f6
Shubham <shubhi6767@gmail.com>
2023-09-22 02:16:37 +0530

    JSX implemented structure

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 let ele = document.querySelector("[id='root']");
+let name = "Tom";
+let year = "1928";
 
 ReactDOM.createRoot(ele).render(
   <div>
     <h1>React tut by techgiddion</h1>
+    <p>2023</p>
     <button>Click Here</button>
+    <h1>{name}</h1>
+    <p>{year}</p>
+    <p>The year is {new Date().getFullYear()}</p>
   </div>
 );
