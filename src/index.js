a9c396b1ca96add58e9da7f92d2c50675ea45091
Shubham <shubhi6767@gmail.com>
2023-09-22 02:36:55 +0530

    inline css implemented

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import ReactDOM from "react-dom/client";
 let ele = document.querySelector("[id='root']");
 let name = "Tom";
 let year = "1928";
+let l1 =
+  "https://www.youtube.com/embed/videoseries?si=_3eYUJKBbtKa39oq&amp;list=PL7tE2k4DDpoc5vmfLq5hU5ajLzuXtqZiD";
 
 ReactDOM.createRoot(ele).render(
   <div>
-    <h1>React tut by techgiddion</h1>
-    <p>2023</p>
+    <h1 className="heading">React tut by techgiddion</h1>
+    <p style={{ color: "grey" }}>2023</p>
     <button>Click Here</button>
     <h1>{name}</h1>
     <p>{year}</p>
     <p>The year is {new Date().getFullYear()}</p>
+
+    <iframe src={l1} title="React Playlist"></iframe>
   </div>
 );
